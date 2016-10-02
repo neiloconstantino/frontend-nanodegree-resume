79dbc33c7d202bbf0cd6ba488adf28d5f7135437
Neil Constantino <neilconstantino@Neils-MacBook-Pro.local>
2016-10-02 12:10:04 -0700

    Initial draft of resumeBuilder.js, add profile image

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,96 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+    "name" : "Neil O. Constantino",
+    "role" : "Software Engineer",
+    "contacts" : {
+        "mobile" : "(909) 720-7080",
+        "email" : "neiloconstantino@gmail.com",
+        "github" : "neiloconstantino",
+        "location" : "Los Angeles"
+    },
+    "welcomeMessage" : "Hello World!",
+    "skills" : ["HTML",  "CSS", "JavaScript", "jQuery", "Bootstrap"],
+    "bioPic" : "images/me.jpg"
+};
+
+var education = {
+    "schools" : [
+        {
+            "name" : "University of Southern California",
+            "city" : "Los Angeles, CA, USA",
+            "degree" : "M.S.",
+            "majors" : ["Computer Science"],
+            "dates" : 2018,
+            "url" : "http://www.usc.edu/"
+        },
+        {
+            "name" : "University of California, Irvine",
+            "city" : "Irvine, CA, USA",
+            "degree" : "B.S.",
+            "majors" : ["Computer Engineering"],
+            "dates" : 2008,
+            "url" : "https://uci.edu/"
+        },
+        {
+            "name" : "University of California, Los Angeles, Extension",
+            "city" : "Los Angeles, CA, USA",
+            "degree" : "Certificate in",
+            "majors" : ["Real Estate w/ Concentration in Investments"],
+            "dates" : 2016,
+            "url" : "https://www.uclaextension.edu/"
+        }
+    ],
+    "onlineCourses" : [
+        {
+            "title" : "Front End Web Developer",
+            "school" : "Udacity",
+            "dates" : 2016,
+            "url" : "https://www.udacity.com/"
+        }
+    ]
+    /*TODO: Fill out additional education (e.g. Air Force Training, etc.)*/
+}
+
+var work = {
+    "jobs" : [
+        {
+            "employer" : "United States Air Force",
+            "title" : "Reservist / Captain",
+            "dates" : "2014 - present"
+        },
+        {
+            "employer" : "United States Air Force",
+            "title" : "Test Manager",
+            "dates" : "2013 - 2014",
+            "description" : "TODO: fill out description"
+        },
+        {
+            "employer" : "United States Air Force",
+            "title" : "Lead Software Engineer",
+            "dates" : "2011 - 2013",
+            "description" : "TODO: fill out description"
+        },
+        {
+            "employer" : "Raytheon",
+            "title" : "Software Quality Engineer",
+            "dates" : "2008 - 2010",
+            "description" : "TODO: fill out description"
+        },
+        {
+            "employer" : "Conexant",
+            "title" : "Verification & Test Engineer",
+            "dates" : "2006 - 2008",
+            "description" : "TODO: fill out description"
+        }
+    ]
+}
+
+var projects = {
+    "projets" : [ 
+        {
+            "title" : "Sample Project 1",
+            "dates" : "2016",
+            "description" : "TODO: Sample project 1 description",
+            "images" : ["TODO: image 1 URL", "TODO: image 2 URL"]
+        }
+    ]
+}
